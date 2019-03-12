9f6da1a2abf483d742ee2ad3d2dcf72d943057f0
Nicole Wilsey Starr <Nicole.WilseyStarr@gmail.com>
2019-03-12 13:47:26 -0500

    added additional friends and favicon

--- friend-finder-app/app/data/friends.js
+++ friend-finder-app/app/data/friends.js
@@ -1,49 +1,293 @@
 
 var friendsArr = [
     {
-        "name": "Ahmed",
-        "photo": "https://imgur.com/nOo4Zb0",
+        "name": "Michael Scott",
+        "photo": "https://pbs.twimg.com/profile_images/378800000188952489/f7c7d214c75419a454a8040e3a763c24.jpeg",
         "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
+            "2",
+            "5",
+            "1",
+            "1",
+            "4",
+            "2",
+            "3",
+            "3",
+            "5",
+            "2"
         ]
     },
     {
-        "name": "Harry Potter",
-        "photo": "https://timedotcom.files.wordpress.com/2014/07/301386_full1.jpg",
+        "name": "Dwight Schrute",
+        "photo": "https://pbs.twimg.com/profile_images/344513261569253648/6552cd6167d7bd7cd3ddbfadff04df59.jpeg",
         "scores": [
-            3,
-            2,
-            4,
-            5,
-            5,
-            3,
-            1,
-            4,
-            1,
-            5
+            "4",
+            "2",
+            "2",
+            "5",
+            "2",
+            "3",
+            "2",
+            "5",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Jim Halpert",
+        "photo": "https://pbs.twimg.com/profile_images/689478700937572356/te4SD8U__400x400.png",
+        "scores": [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "4",
+            "4",
+            "3",
+            "3",
+            "5"
+        ]
+    },
+    {
+        "name": "Kelly Kapoor",
+        "photo": "https://pm1.narvii.com/6410/af695254862ab87a1dbc836f3de9e124d702acc5_128.jpg",
+        "scores": [
+            "5",
+            "5",
+            "1",
+            "1",
+            "5",
+            "2",
+            "2",
+            "3",
+            "4",
+            "5"
+        ]
+    },
+    {
+        "name": "Pam Halpert (Beesly)",
+        "photo": "https://66.media.tumblr.com/avatar_0450bd06008e_128.pnj",
+        "scores": [
+            "5",
+            "5",
+            "2",
+            "4",
+            "2",
+            "4",
+            "4",
+            "3",
+            "3",
+            "5"
+        ]
+    },
+    {
+        "name": "Holly Flax",
+        "photo": "https://res.cloudinary.com/enjoymovie/image/upload/w_256,h_256,c_fill,g_auto:faces/o9xb0rrpxiylala6yafo.jpg",
+        "scores": [
+            "4",
+            "4",
+            "3",
+            "5",
+            "2",
+            "4",
+            "4",
+            "3",
+            "3",
+            "5"
+        ]
+    },
+    {
+        "name": "Angela Martin Schrute",
+        "photo": "https://pm1.narvii.com/7008/eedf918333759b823865454df958e1ee90f48f91r1-350-495v2_128.jpg",
+        "scores": [
+            "2",
+            "1",
+            "3",
+            "5",
+            "2",
+            "4",
+            "1",
+            "5",
+            "2",
+            "4"
+        ]
+    },
+    {
+        "name": "Oscar Martinez",
+        "photo": "https://pm1.narvii.com/6410/dab388ee9d198c6ec9ac3ff4b9a0afa9a5362f99_128.jpg",
+        "scores": [
+            "5",
+            "4",
+            "3",
+            "5",
+            "3",
+            "4",
+            "4",
+            "1",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "Kevin Malone",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRla-Vtb5b_bkxFkaF6e_VBnWRXsFHUMJ772Acvdr_TspxcT72x",
+        "scores": [
+            "5",
+            "4",
+            "2",
+            "1",
+            "1",
+            "1",
+            "3",
+            "2",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Stanley Hudson",
+        "photo": "https://pm1.narvii.com/6410/892a229cc3188c98976ac2bd4f85cc7c5da7d71f_128.jpg",
+        "scores": [
+            "1",
+            "2",
+            "2",
+            "4",
+            "1",
+            "5",
+            "1",
+            "3",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Phyllis Vance (Lapin)",
+        "photo": "https://pbs.twimg.com/profile_images/378800000826878849/f794aee3ea8b5b3a7831f9e2b379f4e6.jpeg",
+        "scores": [
+            "5",
+            "4",
+            "2",
+            "4",
+            "4",
+            "4",
+            "2",
+            "4",
+            "4",
+            "4"
+        ]
+    },
+    {
+        "name": "Andy Bernard",
+        "photo": "https://pm1.narvii.com/6910/f1edf81d6dbf56b6f062b4c5307a5ff12e64589ar1-2048-2048v2_128.jpg",
+        "scores": [
+            "5",
+            "5",
+            "4",
+            "1",
+            "5",
+            "4",
+            "5",
+            "1",
+            "2",
+            "4"
+        ]
+    },
+    {
+        "name": "Toby Flenderson",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1HqgVSMwxDKM4UaIHtQ2Vl-dgcl6OckwGfIaYg9B3VEqzfgj8tw",
+        "scores": [
+            "2",
+            "3",
+            "4",
+            "5",
+            "2",
+            "3",
+            "5",
+            "2",
+            "1",
+            "1"
+        ]
+    },
+    {
+        "name": "Ryan Howard",
+        "photo": "https://pm1.narvii.com/6786/c033cc070ee9c935faa4051e9ffe2877b1ac1553v2_128.jpg",
+        "scores": [
+            "5",
+            "5",
+            "1",
+            "3",
+            "4",
+            "3",
+            "1",
+            "5",
+            "4",
+            "5"
+        ]
+    },
+    {
+        "name": "Creed Braton",
+        "photo": "https://pbs.twimg.com/profile_images/2793670337/2a356501af8944768072835abdb45f77.jpeg",
+        "scores": [
+            "2",
+            "3",
+            "2",
+            "1",
+            "4",
+            "4",
+            "1",
+            "1",
+            "5",
+            "2"
+        ]
+    },
+    {
+        "name": "Meredith Palmer",
+        "photo": "https://pm1.narvii.com/6410/8b11643ddcc8f5b3b21973e35d120ee3b856dd0b_128.jpg",
+        "scores": [
+            "5",
+            "5",
+            "2",
+            "1",
+            "3",
+            "4",
+            "1",
+            "3",
+            "5",
+            "3"
+        ]
+    },
+    {
+        "name": "(Kelly) Erin Hannon",
+        "photo": "https://pm1.narvii.com/6410/fb043b75d1aba9bea0b8f2fe1bfa054cefa75af1_128.jpg",
+        "scores": [
+            "5",
+            "5",
+            "1",
+            "1",
+            "5",
+            "1",
+            "5",
+            "1",
+            "3",
+            "4"
+        ]
+    },
+    {
+        "name": "Robert California",
+        "photo": "https://pm1.narvii.com/6738/cb48d3cb7f63c3961fc1e6dd0b1a0dc6b0d42adfv2_128.jpg",
+        "scores": [
+            "5",
+            "5",
+            "1",
+            "5",
+            "5",
+            "5",
+            "1",
+            "5",
+            "1",
+            "5"
         ]
     }
-
-];
+]
 
 module.exports = friendsArr;
-
-// 6. Determine the user's most compatible friend using the following as a guide:
-
-//    * Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
-//    * With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
-//      * Example:
-//        * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
-//        * User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
-//        * Total Difference: **2 + 1 + 2 =** **_5_**
-//    * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on.
-//    * The closest match will be the user with the least amount of difference.
--- friend-finder-app/routes/htmlRoutes.js
+++ friend-finder-app/routes/htmlRoutes.js
@@ -1,8 +1,3 @@
-// 3. Your `htmlRoutes.js` file should include two routes:
-
-//    * A GET Route to `/survey` which should display the survey page.
-//    * A default, catch-all route that leads to `home.html` which displays the home page.
-
 var path = require("path");
 
 module.exports = function(app) {
